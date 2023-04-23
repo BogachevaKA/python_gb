7fe16d7d834461a4f35a4650d615e74941a72418
BogachevaKA1 <BogachevaKA1@it.mos.ru>
2023-04-23 21:55:49 +0300

    lesson3

--- lesson3/task16.py
+++ lesson3/task16.py
@@ -7,10 +7,14 @@
 #    3
 #    -> 1
 
-N = int(input('Введите количество элементов в массиве: '))
-A = list(range(N))
-A.append(N)  # добавить значение, которое равно N
-A.remove(0)  # удалить значение 0 в массиве
+# N = int(input('Введите количество элементов в массиве: '))
+# A = list(range(N))
+# A.append(N)  # добавить значение, которое равно N
+# A.remove(0)  # удалить значение 0 в массиве
+# print(*A)
+from random import randrange
+N = 10
+A = [randrange(30) for i in range(N)]
 print(*A)
 X = int(input('Введите число, которое необходимо найти в массиве: '))
 num=0
